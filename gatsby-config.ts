cef900d34ae5aa7a1ba2ee46c9fcf875168f9a32
wojciechtobis <wojciechtobis@github.com>
2022-04-07 23:08:57 +0200

    query data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site2`,
+    title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
@@ -13,8 +13,8 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        "name": "pages",
-        "path": "./src/pages/"
+        "name": "blog",
+        path: `./blog`
     },
     __key: "pages"
   }]
