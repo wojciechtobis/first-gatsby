dab89da6b1d09314c792ae6b21327ac99f3b84b0
wojciechtobis <wojciechtobis@github.com>
2022-04-07 18:02:05 +0200

    add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About me</title>
+            <h1>About me</h1>
+            <p>Hi there! I'm proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+
+export default AboutPage
